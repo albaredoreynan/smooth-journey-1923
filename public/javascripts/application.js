fbb5c4b409bbd3d1d6bbada0e9e28904496a76f1
Tristan Peralta <tristanperalta@gmail.com>
2011-11-09 21:30:48 +0800

    Remove unused file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+//
 function startCalc(){
   interval = setInterval("calc()",1);
 }
 
 function calc(){
   one = document.new_categorysale.categorysale[csrows_attributes][0][cs_amount].value;
-  two = document.new_categorysale.categorysale[csrows_attributes][1][cs_amount].value; 
+  two = document.new_categorysale.categorysale[csrows_attributes][1][cs_amount].value;
   document.new_categorysale.categorysale[cs_total_amount].value = (one * 1) + (two * 1);
-  
+
 }
 
 function stopCalc(){
