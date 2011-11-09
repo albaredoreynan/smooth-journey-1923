ae4413c61e1fcfa406c5a9709830772d8ec3c35a
Tristan Peralta <tristanperalta@gmail.com>
2011-11-09 10:07:21 +0800

    Allow remember_me for mass-assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
