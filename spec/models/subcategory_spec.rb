3c80b2c15c8bff792b4eefc8605472065742fc71
Tristan Peralta <tristanperalta@gmail.com>
2011-11-23 10:34:58 +0800

    Subcategory - category_id is now required

--- spec/models/subcategory_spec.rb
+++ spec/models/subcategory_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe Subcategory do
 
   context 'Validation' do
+    before do
+      @subcategory = Subcategory.new
+    end
+
     it 'should be invalid without name' do
-      subcategory = Subcategory.new
-      subcategory.should have(1).error_on :name
+      @subcategory.should have(1).error_on :name
+    end
+
+    it 'should be invalid without a category' do
+      @subcategory.should have(1).error_on :category_id
     end
   end
 end
