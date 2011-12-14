c7012aedd1f2797a9ddbe592bd3c47730dc5e7a4
Tristan Peralta <tristanperalta@gmail.com>
2011-12-14 18:21:08 +0800

    Clean up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rrbs::Application.routes.draw do
+  devise_for :users
 
   match '/purchases/search' => 'purchases#index'
 
@@ -18,23 +19,11 @@ Rrbs::Application.routes.draw do
 
   resources :companies
 
-  resources :restaurantcategories
-
   resources :restaurants
 
   resources :roles
 
   resources :suppliers
-
-  resources :reporttemplates
-
-  devise_for :users
-
-  resources :csrows
-
-  resources :ecrows
-
-  resources :ssrows
 
   resources :settlement_types
 
@@ -48,9 +37,9 @@ Rrbs::Application.routes.draw do
 
   resources :sales_reports
 
-  resources :purchase_items
-
-  resources :purchases
+  resources :purchases do
+    resource :purchase_items
+  end
 
   resources :inventoryitems
 
@@ -60,38 +49,19 @@ Rrbs::Application.routes.draw do
 
   resources :users
 
-  resources :endcounts
-
-  match "/purchaseitems/search" => "purchaseitems#index"
-
-  match "/purchaseitems/index" => "purchaseitems#index", :as => :purchaseitems_index
-
-  #match "/endcounts/index" => "endcounts#index", :as => :endcounts_index #don't include, messes up search then save
+  resources :endcounts do
+    collection do
+      get 'search'
+    end
+  end
 
   match "/reports" => "reports#index"
-
-  #resources :purchaseitems, :collection => { :savemultiple => :put }
-
-  match '/purchaseitems/savemultiple' => "purchaseitems#index", :collection => { :savemultiple => :put }
 
   match '/purchases/savemultiple' => "purchases#index", :collection => { :savemultiple => :put }
 
   match '/endcounts/savechecked' => "endcounts#index", :collection => { :savechecked => :put }
 
   match '/sales/savemultiple' => "sales#sales_by_server", :collection => { :savemultiple => :put }
-  #resources :endcounts, :collection => { :savechecked => :put }
-
-  resources :purchaseitems do
-    collection do
-      get 'search'
-    end
-  end
-
-  resources :endcounts do
-    collection do
-      get 'search'
-    end
-  end
 
   match '/sales_reports/daily_sales' => "sales_reports#daily_sales", :as => :sales_reports_daily_sales
 
@@ -126,61 +96,4 @@ Rrbs::Application.routes.draw do
   end
 
   root :to => 'home#index'
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
