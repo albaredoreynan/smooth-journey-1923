5edc5f873d9e2f200c22796782bc61278881dd75
Tristan Peralta <tristanperalta@gmail.com>
2012-01-05 18:15:33 +0800

    Style for "as of" text on period end

--- app/helpers/endcounts_helper.rb
+++ app/helpers/endcounts_helper.rb
@@ -1,7 +1,12 @@
 module EndcountsHelper
   def endcount_title(date=nil)
     et = "View Endcounts"
-    et += " as of #{date}" if date
-    return et
+    et += " " + content_tag(:span, "as of #{date}",
+      :style => %Q{
+        font-style: italic;
+        color: #848484;
+        font-size: 18px;
+      }) if date
+    return raw et
   end
 end
