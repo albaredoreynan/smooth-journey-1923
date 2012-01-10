9e6f0d6cccffba9e58b61401a2fc9052d6dd6384
Tristan Peralta <tristanperalta@gmail.com>
2012-01-10 14:06:53 +0800

    Filter endcount_items for branch manager

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,30 +3,7 @@ class ReportsController < ApplicationController
   set_tab :reports
 
   def index
-    @endcount = Endcount.new(EndcountItem.all, Date.today, Date.today.beginning_of_month)
-  end
-
-  def show
-   #insert code
-  end
-
-  def purchasereports
-    @purchases = Purchase.all
-
-    if params[:commit]=="Search"
-        startdate = params[:start]['(1i)']+ '-' + params[:start]['(2i)'] + '-' + params[:start]['(3i)']
-        enddate = params[:end]['(1i)']+ '-' + params[:end]['(2i)'] + '-' + params[:end]['(3i)']
-
-        @duration = startdate + ' to ' + enddate
-        @purchases = Purchase.search_by_date(startdate, enddate)
-        #@categories = Category.all
-    elsif params[:commit]=="Search by supplier"
-        @purchases = Purchase.search_by_supplier(params[:supplier][:supplier_id])
-    elsif params[:commit]=="Search by invoice"
-    elsif params[:commit]=="Search by category"
-        #@purchases = Purchase.search_by_category(params[:category][:category_id])
-    elsif params[:commit]=="Search by subcategory"
-    elsif params[:commit]=="Search by item"
-    end
+    endcount_items = EndcountItem.where(current_ability.attributes_for(:read, Item))
+    @endcount = Endcount.new(endcount_items, Date.today, Date.today.beginning_of_month)
   end
 end
