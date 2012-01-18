fc6b98d8410c11d44b615bbc88e78b0c973840e2
Tristan Peralta <tristanperalta@gmail.com>
2012-01-18 16:34:54 +0800

    Group endcount items by subcategory

--- app/controllers/reports/endcount_reports_controller.rb
+++ app/controllers/reports/endcount_reports_controller.rb
@@ -3,7 +3,7 @@ class Reports::EndcountReportsController < ApplicationController
   set_tab :reports
 
   def index
-    endcount_items = EndcountItem.where(current_ability.attributes_for(:read, Item))
+    endcount_items = EndcountItem.includes(:subcategory).where(current_ability.attributes_for(:read, Item))
     if params[:date]
       ending_date = Date.new(params[:date][:year].to_i, params[:date][:month].to_i)
     else
