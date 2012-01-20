f6f36c66697667969971be657c2cfcad876ee9a4
Tristan Peralta <tristanperalta@gmail.com>
2012-01-20 16:23:47 +0800

    Remove unused controllers

--- app/controllers/sales_reports_controller.rb
+++
@@ -1,7 +0,0 @@
-class SalesReportsController < ApplicationController
-
-  def index
-    @category_sales = Categorysale.where("date = ?",Date.today.to_s)
-    @settlement_sales = SettlementSale.where("ss_date = ?",Date.today.to_s)
-  end
-end
