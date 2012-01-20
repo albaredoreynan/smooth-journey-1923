cc5c7776cad86527ac50a367327bf9dba544c2f5
Tristan Peralta <tristanperalta@gmail.com>
2012-01-20 16:23:47 +0800

    Remove unused controllers

--- spec/requests/settlement_sales_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "SettlementSales" do
-  describe "GET /settlement_sales" do
-    it "works! (now write some real specs)" do
-      get settlement_sales_path
-    end
-  end
-end
