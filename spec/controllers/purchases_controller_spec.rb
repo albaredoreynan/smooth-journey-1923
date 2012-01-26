2a2743eb89279e31ed45b81d3667c5a17a0d7a49
Tristan Peralta <tristanperalta@gmail.com>
2012-01-26 15:55:22 +0800

    Align (soft) tab on purchases_controller_spec

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -6,13 +6,6 @@ describe PurchasesController do
     login_admin
 
     describe "GET index" do
-      it "should show all non-draft purchases" do
-        purchase = FactoryGirl.create(:purchase, :save_as_draft => false)
-        draft_purchase = FactoryGirl.create(:purchase, :save_as_draft => true)
-        get 'index'
-        assigns[:purchases].should eq [purchase]
-      end
-
       context "Search" do
         before do
           @start_date = 5.days.ago
@@ -56,6 +49,13 @@ describe PurchasesController do
           get 'index', :start_date => '', :end_date => ''
           assigns[:purchases].should eq [@purchases.last, @purchases.first]
         end
+      end
+
+      it "should show all non-draft purchases" do
+        purchase = FactoryGirl.create(:purchase, :save_as_draft => false)
+        draft_purchase = FactoryGirl.create(:purchase, :save_as_draft => true)
+        get 'index'
+        assigns[:purchases].should eq [purchase]
       end
     end
 
@@ -128,33 +128,34 @@ describe PurchasesController do
         post :create, :purchase => @post_params
         @purchase.reload.save_as_draft.should eq false
       end
+    end
 
-      context 'PUT #update' do
-        before do
-          @purchase = FactoryGirl.create(:purchase)
-          branch = @purchase.branch
-          supplier = @purchase.branch
-          @put_params = { :supplier_id => supplier.id, :branch_id => branch.id }
-        end
+    describe 'PUT #update' do
+      before do
+        @purchase = FactoryGirl.create(:purchase)
+        branch = @purchase.branch
+        supplier = @purchase.branch
+        @put_params = { :supplier_id => supplier.id, :branch_id => branch.id }
+      end
 
-        after do
-          session.delete :purchase
-        end
+      after do
+        session.delete :purchase
+      end
 
-        it 'should set purchase as non-draft' do
-          @purchase.update_attribute(:save_as_draft, true)
-          session[:purchase] = @purchase.id
-          put 'update', :id => @purchase.id, :purchase => @put_params
-          @purchase.reload.save_as_draft.should eq false
-        end
+      it 'should set purchase as non-draft' do
+        @purchase.update_attribute(:save_as_draft, true)
+        session[:purchase] = @purchase.id
+        put 'update', :id => @purchase.id, :purchase => @put_params
+        @purchase.reload.save_as_draft.should eq false
+      end
 
-        it 'should clear the purchase session' do
-          session[:purchase] = @purchase.id
-          put 'update', :id => @purchase.id, :purchase => @put_params
-          session[:purchase].should be_nil
-        end
+      it 'should clear the purchase session' do
+        session[:purchase] = @purchase.id
+        put 'update', :id => @purchase.id, :purchase => @put_params
+        session[:purchase].should be_nil
       end
     end
+
   end
 
   context 'as branch manager' do
