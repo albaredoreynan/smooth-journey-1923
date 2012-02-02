5d72987c5fbb090522a8e966023021d69be6d512
Tristan Peralta <tristanperalta@gmail.com>
2012-02-02 11:19:24 +0800

    Endcount report in PDF Format

--- app/controllers/reports/endcount_reports_controller.rb
+++ app/controllers/reports/endcount_reports_controller.rb
@@ -5,16 +5,20 @@ class Reports::EndcountReportsController < ReportsController
   def index
     endcount_items = EndcountItem.includes(:subcategory).where(current_ability.attributes_for(:read, Item))
     if params[:date]
-      ending_date = Date.new(params[:date][:year].to_i, params[:date][:month].to_i)
+      @ending_date = Date.new(params[:date][:year].to_i, params[:date][:month].to_i)
     else
-      ending_date = Date.today
+      @ending_date = Date.today
     end
-    @endcount = Endcount.new(endcount_items, ending_date)
+    @endcount = Endcount.new(endcount_items, @ending_date)
 
     respond_to do |wants|
       wants.html
       wants.csv do
         render_csv(params[:controller])
+      end
+      wants.pdf do
+        headers['Content-Disposition'] = "attachment; filename=\"#{params[:controller]}\""
+        render :layout => false
       end
     end
   end
