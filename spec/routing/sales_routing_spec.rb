14c1a83d25837cf9d9612fb16dc47d8b7ab73b85
Tristan Peralta <tristanperalta@gmail.com>
2012-02-20 16:10:18 +0800

    Sales routing and test cases

---
+++ spec/routing/sales_routing_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe SalesController do
+
+  it 'should route #index' do
+    { :get => 'sales' }.should route_to(:controller => 'sales', :action => 'index')
+  end
+
+  it 'should route sales by categories' do
+    { :get => 'sales/categories' }.should route_to(:controller => 'sales', :action => 'index')
+  end
+
+  it 'should route sales by settlement types' do
+    { :get => 'sales/settlement_types' }.should route_to(:controller => 'sales', :action => 'settlement_types')
+  end
+end
