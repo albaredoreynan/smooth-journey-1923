711f54e22fcd785dcc4cd2f3b0fbe35dd8341ded
Tristan Peralta <tristanperalta@gmail.com>
2012-02-22 16:18:54 +0800

    delete invalid total calculations on sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -48,42 +48,6 @@ class Sale < ActiveRecord::Base
     return finder
   end
 
-  def self.total_void
-    all.map(&:void).sum
-  end
-
-  def self.total_vat
-    all.map(&:vat).sum
-  end
-
-  def self.total_service_charge
-    all.map(&:service_charge).sum
-  end
-
-  def self.total_revenue
-    all.map(&:total_revenue_cs).sum
-  end
-
-  def self.total_customer_count
-    all.map(&:customer_count).sum
-  end
-
-  def self.total_transaction_count
-    all.map(&:transaction_count).sum
-  end
-
-  def self.total_gross_total_ss
-    all.map(&:gross_total_ss).sum
-  end
-
-  def self.total_net_total_ss
-    all.map(&:net_total_ss).sum
-  end
-
-  def self.gross_sales_sum(date)
-    where("date = ?",date).map(&:total_amount_cs).sum
-  end
-
   private
   def check_total
     errors.add(:base, "The total doesn't add up.") if category_total != settlement_type_total
