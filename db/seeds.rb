54ff73455c866f4a1cc91fb18e9fe308cd764dca
Tristan Peralta <tristanperalta@gmail.com>
2012-02-29 10:53:09 +0800

    Minor edit on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,9 @@ vince = User.create(:username => 'vince', :email => 'vincepinto@example.com', :p
 tristan = User.create(:username => 'tristan', :email => 'tristan@example.com', :password => 'password')
 reynan = User.create(:username => 'reynan', :email => 'reynan@example.com', :password => 'reynan')
 
+Role.create(:name => 'admin', :user => reynan)
+
 company = Company.create(:name => 'Test Company')
 
 Role.create(:name => 'client', :user => vince, :company => company)
-
-Role.create(:name => 'admin', :user => reynan, :company => company)
-
 Role.create(:name => 'client', :user => tristan, :company => company)
-
