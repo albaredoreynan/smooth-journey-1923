3594f87c05dfd22cc71a44313b720d76a5c88bd2
Tristan Peralta <tristanperalta@gmail.com>
2012-03-05 00:04:58 +0800

    remove Item update on migration file

--- db/migrate/20120304152328_add_restaurant_id_on_item.rb
+++ db/migrate/20120304152328_add_restaurant_id_on_item.rb
@@ -2,11 +2,6 @@ class AddRestaurantIdOnItem < ActiveRecord::Migration
   def up
     add_column :items, :restaurant_id, :integer
     add_index :items, :restaurant_id
-
-    Item.all.each do |item|
-      item.restaurant = item.branch.restaurant
-      item.save
-    end
   end
 
   def down
