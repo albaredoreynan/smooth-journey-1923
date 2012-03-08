490550649b36a814c36421079cd3c61a75e4ab06
Tristan Peralta <tristanperalta@gmail.com>
2012-03-08 17:56:24 +0800

    More data on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@ Role.create(:name => 'admin', :user => reynan)
 
 company = Company.create(:name => 'Test Company')
 
+restaurant = FactoryGirl.create(:restaurant, :company => company)
+branch = FactoryGirl.create(:branch, :restaurant => restaurant)
+
 Role.create(:name => 'client', :user => vince, :company => company)
 Role.create(:name => 'client', :user => tristan, :company => company)
+
+category = FactoryGirl.create(:category, :name => 'Category A', :restaurant => restaurant)
+subcategory = FactoryGircl.create(:subcategory, :name => 'Subcategory A', :category => category)
