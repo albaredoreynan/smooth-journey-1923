b4990ad37732ff838dc3e656644b2718531e2abf
chezca1228 <lo_r_can@yahoo.com>
2012-05-21 16:57:44 +0800

    added the branch filtering and fixed the pdf and csv generation in purchase reports modules

--- app/controllers/reports/purchase_reports_controller.rb
+++ app/controllers/reports/purchase_reports_controller.rb
@@ -3,23 +3,34 @@ class Reports::PurchaseReportsController < ReportsController
   set_tab :purchase_reports
 
   def index
-    @start_date = params[:start_date] || Date.today.beginning_of_month
-    @end_date = params[:end_date] || Date.today
+    if params[:start_date].blank? && params[:end_date].blank?
+      @start_date = Date.today.beginning_of_month.to_s
+      @end_date = Date.today.to_s
+    else
+      @start_date = params[:start_date]
+      @end_date = params[:end_date]
+    end
     
-    @supplier = params[:supplier] || ""
-    @invoice_number = params[:invoice_number] || ""
-    @item = params[:item] || ""
-    @subcategory = params[:subcategory] || ""
     
+    if current_user.branch?
+      branch_id = @current_branch.id
+    else
+      @branch = Branch.accessible_by(current_ability).first
+      branch_id = params[:branch_id] || @branch.id
+    end
+    
+    puts ">>>>date"
+    puts @start_date +"--"+ @end_date  
     @purchase_items = PurchaseItem.
       accessible_by(current_ability).
       search(
         :start_date => @start_date,
         :end_date => @end_date,
-        :supplier => @supplier,
-        :invoice_number => @invoice_number,
-        :item => @item,
-        :subcategory => @subcategory
+        :supplier => params[:supplier],
+        :invoice_number => params[:invoice_number],
+        :item => params[:item],
+        :subcategory => params[:subcategory],
+        :branch_id => branch_id
       ).
       joins(:purchase).where('purchases.deleted_at IS NULL').group_by do |pi|
       pi.item.subcategory ||= Subcategory.new(:name => '(No subcategory)')
