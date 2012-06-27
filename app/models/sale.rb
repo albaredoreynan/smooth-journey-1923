6ad7dbbb160b4ca9443ced0c8fe33414123773ce
chezca1228 <lo_r_can@yahoo.com>
2012-06-27 14:56:34 +0800

    files for sales month to date

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -70,13 +70,56 @@ class Sale < ActiveRecord::Base
     return finder
   end
 
+  def month_to_date
+    Sale.find(:all, :conditions => ['sale_date >= ? and sale_date <= ?', Date.today.beginning_of_month, Date.today] )
+  end
+  
+  def month_to_date_customer_count
+    month_to_date.map(&:customer_count).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_transaction_count
+    month_to_date.map(&:transaction_count).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_delivery_sales
+    month_to_date.map(&:delivery_sales).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_cash_in_drawer
+    month_to_date.map(&:cash_in_drawer).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_gc_sales
+    month_to_date.map(&:gc_sales).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_other_income
+    month_to_date.map(&:other_income).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_vat
+    month_to_date.map(&:vat).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_gc_redeemed
+    month_to_date.map(&:gc_redeemed).reject(&:nil?).inject(:+) || 0
+  end
+  
+  def month_to_date_service_charge
+    month_to_date.map(&:service_charge).reject(&:nil?).inject(:+) || 0
+  end
+
+  def month_to_date_cash_for_deposit
+    month_to_date_cash_in_drawer + month_to_date_gc_sales + month_to_date_other_income
+  end
   private
   def category_and_settlement_totals_should_be_equal
     unless total_revenues == total_settlement_type_sales && server_sale_total == cash_for_deposit
       errors.add(:base, 'category sale and settlement type sales should be equal')
     end
   end
-
+  
   def cash_for_deposit_totals_should_be_equal
     unless server_sale_total == cash_for_deposit
       errors.add(:base, 'server sale and cash for deposit should be equal')
