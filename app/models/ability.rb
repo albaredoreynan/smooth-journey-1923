a05a3797760774ad2fbf17fc498f82384a905707
chezca1228 <lo_r_can@yahoo.com>
2012-07-27 14:07:14 +0800

    copy the ability of branch and set it to client

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -84,35 +84,111 @@ class Ability
 
 
     when 'client'
-      company_id = user.companies.first
-      can [:read, :edit, :update], Company, :id => company_id
-      can :new,    Branch
-      can :manage, Branch, :restaurant => { :company => { :id => company_id } }
-      can [:new, :create], Category
-      can :manage, Category, :restaurant => { :company => { :id => company_id } }
-      can :manage, Conversion
-      can :manage, Endcount
-      can :manage, [ EndcountItem ], :restaurant => { :company => { :id => company_id } }
-      can :manage, [ Item ], :restaurant => { :company => { :id => company_id } }
-      can :manage, Purchase, :branch => { :restaurant => { :company => { :id => company_id } } }
-      can :manage, PurchaseItem, :purchase => { :branch => { :restaurant => { :company => { :id => company_id } } } }
-      can :manage, Restaurant, :company_id => company_id
-      can :manage, Sale, :branch => { :restaurant => { :company => { :id => company_id } } }
-      can :manage, Server, :branch => { :restaurant => { :company => { :id => company_id } } }
-      can [:new, :create], Server
-      can :manage, SaleCategory, :restaurant => { :company_id => company_id }
-      can :manage, SettlementType, :restaurant => { :company => { :id => company_id } } 
-      can [:new, :create], Subcategory
-      can :manage, Subcategory, :category => { :restaurant => { :company => { :id => company_id } } }
-      can [:new, :create], SettlementType
-      can :manage, SettlementType, :restaurant => { :company => { :id => company_id } } 
-      can :manage, Supplier, :company_id =>  company_id
-      can :new,    Unit
-      can :manage, Unit, :restaurant => { :company => { :id => company_id } }
-      can :new,    Users
-      can :manage, User, :roles => { :company => { :id => company_id } }
-      can :manage, Unit, :restaurant => { :company => { :id => company_id } }
+      
+      #company_id = user.companies.first
+      #can [:read, :edit, :update], Company, :id => company_id
+      #can :new,    Branch
+      #can :manage, Branch, :restaurant => { :company => { :id => company_id } }
+      #can [:new, :create], Category
+      #can :manage, Category, :restaurant => { :company => { :id => company_id } }
+      #can :manage, Conversion
+      #can :manage, Endcount
+      #can :manage, [ EndcountItem ], :restaurant => { :company => { :id => company_id } }
+      #can :manage, [ Item ], :restaurant => { :company => { :id => company_id } }
+      #can :manage, Purchase, :branch => { :restaurant => { :company => { :id => company_id } } }
+      #can :manage, PurchaseItem, :purchase => { :branch => { :restaurant => { :company => { :id => company_id } } } }
+      #can :manage, Restaurant, :company_id => company_id
+      #can :manage, Sale, :branch => { :restaurant => { :company => { :id => company_id } } }
+      #can :manage, Server, :branch => { :restaurant => { :company => { :id => company_id } } }
+      #can [:new, :create], Server
+      #can :manage, SaleCategory, :restaurant => { :company_id => company_id }
+      #can :manage, SettlementType, :restaurant => { :company => { :id => company_id } } 
+      #can [:new, :create], Subcategory
+      #can :manage, Subcategory, :category => { :restaurant => { :company => { :id => company_id } } }
+      #can [:new, :create], SettlementType
+      #can :manage, SettlementType, :restaurant => { :company => { :id => company_id } } 
+      #can :manage, Supplier, :company_id =>  company_id
+      #can :new,    Unit
+      #can :manage, Unit, :restaurant => { :company => { :id => company_id } }
+      #can :new,    Users
+      #can :manage, User, :roles => { :company => { :id => company_id } }
+      #can :manage, Unit, :restaurant => { :company => { :id => company_id } }
 
+      branch = user.branches.first
+
+      # Purchase
+      can :new, Purchase
+      can [:read, :create], Purchase, :branch_id => branch.id
+      can :update, Purchase do |purchase|
+        purchase.branch == branch
+      end
+
+      cannot [:edit, :update], Purchase do |purchase|
+        setting = user.settings
+        seconds_in_minute = 60
+        purchase.created_at < Time.now - setting[:lock_module_in].to_i * seconds_in_minute
+      end
+      
+      can :manage, PurchaseItem, :purchase => { :branch_id => branch.id }
+      
+      # Inventory Item
+      can :manage, Endcount
+      can :manage, [EndcountItem, Item], :restaurant_id => branch.restaurant.id
+      can :update_count, Item do |item|
+        item.entry_date < Date.today - 1.day && item.branch == branch
+      end
+
+      # Item
+      cannot [:create, :destroy, :edit, :update], Item
+
+      # Branch
+      can :read, Branch, :id => branch.id
+      cannot [:create, :destroy, :edit, :update], Branch, :id => branch.id
+
+      # Category
+      can :read, Category, :restaurant_id => branch.restaurant
+      cannot [:create, :edit, :update, :destroy], Category, :restaurant_id => branch.restaurant
+
+      # Conversion
+      can :read, Conversion
+      cannot [:create, :edit, :update, :destroy], Conversion
+
+      # Currency
+      can :read, Currency
+      cannot [:create, :edit, :update, :destroy], Currency
+
+      # Sale
+      can :manage, Sale, :branch_id => branch.id
+      
+      # Sales Category
+      can :read, SaleCategory, :restaurant_id => branch.restaurant
+      
+      # Settlement Type
+      can :read, SettlementType, :restaurant_id => branch.restaurant
+      cannot [:create, :edit, :update, :destroy], SettlementType, :restaurant_id => branch.restaurant
+
+      # Server
+      can :read, Server, :branch_id => branch.id
+      cannot [:create, :edit, :update, :destroy], Server, :branch_id => branch.id
+
+      # Subcategory
+      can :read, Subcategory, :category => { :restaurant_id => branch.restaurant }
+      cannot [:create, :edit, :update, :destroy], Subcategory, :category => { :restaurant_id => branch.restaurant }
+
+      # Supplier
+      can :read, Supplier, :company => { :id => branch.restaurant.company.id }
+      cannot [:create, :edit, :update, :destroy], Supplier, :company => { :id => branch.restaurant.company.id }
+
+      # Unit
+      can :read, Unit, :restaurant_id => branch.restaurant
+      cannot [:create, :edit, :update, :destroy], Unit, :restaurant_id => branch.restaurant
+
+      can :read, User, :roles => { :company => { :id => user.roles.first.company.id } }
+      # only allow edit his own user info
+      can [:edit, :update], User do |usr|
+        user == usr
+      end
+      cannot [:create, :destroy], User, :roles => { :company => { :id => user.roles.first.company.id } }
     when 'accounting'
       can [:index, :read], :all
 
