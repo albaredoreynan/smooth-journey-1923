0e1445c3524870441046071b1c6035a6c230e13d
chezca1228 <lo_r_can@yahoo.com>
2012-08-23 12:26:32 +0800

    fixes in sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -31,7 +31,7 @@ class Sale < ActiveRecord::Base
   end
 
   def settlement_type_total
-    settlement_type_sales.map(&:amount).reject(&:nil?).inject(:+) || 0
+    settlement_type_sales.map(&:amount).reject(&:nil?).inject(:+).to_f || 0
   end
 
   def server_sale_total
@@ -48,11 +48,11 @@ class Sale < ActiveRecord::Base
 
   def total_settlement_type_sales
     #settlement_type_total + gc_redeemed + delivery_sales 
-    (settlement_type_total.to_f) + gc_redeemed + delivery_sales + cash_in_drawer 
+    settlement_type_total + gc_redeemed + delivery_sales + cash_in_drawer 
   end
 
   def cash_for_deposit
-    (cash_in_drawer + gc_sales + other_income)
+    cash_in_drawer + gc_sales + other_income
     #gc_sales + other_income
   end
 
